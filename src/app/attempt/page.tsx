788dbb1cbc11f915c274aa0f0e01562aa66a4c72
afolabi-ola <148984714+afolabi-ola@users.noreply.github.com>
2025-11-05 21:31:22 +0100

    fixed param issues on test/id page

--- src/app/attempt/page.tsx
+++ src/app/attempt/page.tsx
@@ -1,10 +1,5 @@
-// app/(student)/tests/[id]/page.tsx
 import { redirect } from 'next/navigation';
 
-interface Params {
-  params: { sessionId: string };
-}
-
-export default function Page({ params }: Params) {
-  redirect(`/attempt/${params.sessionId}`);
+export default function Page() {
+  redirect(`/tests`);
 }
